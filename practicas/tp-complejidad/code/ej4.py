22ed068b2079c9d0529df28a2b8decf72d82403a
LP05 <spgit0507@gmail.com>
2026-08-22 10:03:52 -0300

    complejidad corregido

--- practicas/tp-complejidad/code/ej4.py
+++ practicas/tp-complejidad/code/ej4.py
@@ -13,6 +13,8 @@ def MitadMenores(A:list)->list:
         else:
             resto.append(i)
 
-    # Coloca la mitad de cada grupo antes y la otra mitad después del primer elemento. Se usa round para la lista resto y se trunca la lista menores
-    #por si ambas tienen un número impar de elementos, y si usaramos la misma operación para las dos, el pivote no quedaría al medio.
+# Coloca la mitad de cada grupo antes y la otra mitad después del primer elemento. 
+# Se usa round para la lista resto y se trunca la lista menores debido a que si ambas tuvieran un número impar de elementos y 
+# usaramos la misma operación para las dos, el pivote no quedaría al medio.
+
     return menores[:len(menores)//2] + resto[:round(len(resto)/2)] + [A[0]] + menores[len(menores)//2:] + resto[round(len(resto)/2):]
